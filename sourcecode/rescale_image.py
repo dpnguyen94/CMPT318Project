60bfe0c04e6301aa2c6c7bd38c0d5d10d642e297
pdnguyen <pdnguyen@sfu.ca>
2017-08-04 21:55:00 -0700

    Upload rescale_image file

---
+++ sourcecode/rescale_image.py
@@ -0,0 +1,16 @@
+import glob
+import os
+
+from PIL import Image
+
+katkam_path = 'katkam-scaled'
+
+katkam_files = glob.glob(katkam_path + '/*.jpg')
+for file in katkam_files:
+    img = Image.open(file)
+    new_width = 128
+    new_height = 92
+    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    
+    file_name = os.path.basename(file)
+    img.save('katkam-rescaled/' + file_name)
